d076f1e5089f3db2d7fcc30366a575a766e277fe
Ritvio <133638300+Ritvio@users.noreply.github.com>
2025-06-28 13:11:00 +0530

    Update cash.js

--- commands/cash.js
+++ commands/cash.js
@@ -1,12 +1,10 @@
-const { getBalance } = require("../utils/currency");
+const { getBalance } = require("../utils/userDB");
 
 module.exports = {
     name: "cash",
     async execute(message) {
         const userId = message.author.id;
-        const balance = await getBalance(userId); // Await MongoDB fetch
-        message.channel.send(
-            `**${message.member.displayName}**, you currently have **__${balance.toLocaleString("en-US")}__** <:indo_cash:1337749441289129994>`
-        );
+        const balance = await getBalance(userId);
+        message.channel.send(`**${message.member.displayName}**, you currently have **__${balance.toLocaleString("en-US")}__** <:indo_cash:1337749441289129994>`);
     }
 };
